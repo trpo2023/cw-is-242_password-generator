e2fd4fe6fcca2bdc1623d6605833c11587fc283f
xcredo <vlad.kimosov1@mail.ru>
2023-05-10 03:00:01 +0700

    Add gen.c file

---
+++ src/LibPwgen/gen.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "libgen.h"
+
+int main()
+{
+    int dlin;
+    int up;
+    int down;
+    int spets;
+    int kolvo;
+    Ui(&dlin, &up, &down, &spets, &kolvo);
+    return 0;
+}
