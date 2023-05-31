0fe42e1d65c5172fc1ffb81b62bdb7065af5ad95
xcredo <vlad.kimosov1@mail.ru>
2023-05-31 23:05:24 +0700

    continue setting up tests

--- test/test.c
+++ test/test.c
@@ -57,35 +57,35 @@ CTEST(wtime, returns_current_wall_time_in_seconds)
 // }
 
 
-CTEST(Good, modifies_array_of_good_characters_according_to_libgen_structure)
-{
-    libgen t = { 8, 1, 0, 1, 5 };
+// CTEST(Good, modifies_array_of_good_characters_according_to_libgen_structure)
+// {
+//     libgen t = { 8, 1, 0, 1, 5 };
 
-    int good[122];
-    for (int i = 33; i < 122; i++) {
-        good[i] = 1;
-    }
+//     int good[122];
+//     for (int i = 33; i < 122; i++) {
+//         good[i] = 1;
+//     }
 
-    Good(&t);
+//     Good(&t);
 
-    ASSERT_FALSE(good['A']);
-    ASSERT_TRUE(good['a']);
-    ASSERT_TRUE(good['!']);
-    ASSERT_TRUE(good['@']);
-    ASSERT_TRUE(good['#']);
-    ASSERT_TRUE(good['$']);
-    ASSERT_TRUE(good['%']);
-    ASSERT_TRUE(good['^']);
-    ASSERT_TRUE(good['&']);
-    ASSERT_TRUE(good['*']);
-    ASSERT_FALSE(good['_']);
-    ASSERT_FALSE(good['`']);
+//     ASSERT_FALSE(good['A']);
+//     ASSERT_TRUE(good['a']);
+//     ASSERT_TRUE(good['!']);
+//     ASSERT_TRUE(good['@']);
+//     ASSERT_TRUE(good['#']);
+//     ASSERT_TRUE(good['$']);
+//     ASSERT_TRUE(good['%']);
+//     ASSERT_TRUE(good['^']);
+//     ASSERT_TRUE(good['&']);
+//     ASSERT_TRUE(good['*']);
+//     ASSERT_FALSE(good['_']);
+//     ASSERT_FALSE(good['`']);
 
-    // Другие символы не должны быть изменены
-    ASSERT_TRUE(good['B']);
-    ASSERT_TRUE(good['z']);
-    ASSERT_TRUE(good['~']);
-}
+//     // Другие символы не должны быть изменены
+//     ASSERT_TRUE(good['B']);
+//     ASSERT_TRUE(good['z']);
+//     ASSERT_TRUE(good['~']);
+// }
 
 
 int is_good_password(char* password, size_t len, int* good)
@@ -119,36 +119,59 @@ int count_occurrences(const char* str, const char* substr) {
     return count;
 }
 
-CTEST(Generation, generates_multiple_passwords_according_to_libgen_structure_and_array_of_good_characters)
-{
-    libgen t = { 8, 1, 0, 1, 5 };
+// CTEST(Generation, generates_multiple_passwords_according_to_libgen_structure_and_array_of_good_characters)
+// {
+//     libgen t = { 8, 1, 0, 1, 5 };
 
-    int good[123];
-    for (int i = 33; i <= 122; i++) {
+//     int good[123];
+//     for (int i = 33; i <= 122; i++) {
+//         good[i] = 1;
+//     }
+
+//     // Перенаправляю stdout и захватываю вывод
+//     char output[1000];
+//     FILE* mockstdout = fmemopen(output, sizeof(output), "w");
+//     FILE* realstdout = stdout;
+//     stdout = mockstdout;
+
+//     // Вызов тестируемой функции
+//     Generation(&t, good);
+
+//     // Восстанавливаю stdout
+//     fflush(stdout);
+//     stdout = realstdout;
+
+//     // Проверяю, что было сгенерировано 5 паролей длиной 8
+//     ASSERT_EQUAL(5, count_occurrences(output, "Password"));
+//     ASSERT_EQUAL(40, strlen(strstr(output, "Password")));
+
+//     // Проверяю, что все символы в каждом пароле подходят
+//     char* p = strtok(output, "\n");
+//     while (p != NULL) {
+//         ASSERT_TRUE(is_good_password(p, t.dlin, good));
+//         p = strtok(NULL, "\n");
+//     }
+// }
+
+CTEST(test_generation, output_test) {
+    // создаем структуру libgen для передачи её в функцию Generation
+    libgen* t = (libgen*)malloc(sizeof(libgen));
+    t->kolvo = 2;
+    t->dlin = 5;
+
+    // заполняем массив good значениями 1
+    int good[1000];
+    for (int i = 0; i < 1000; i++) {
         good[i] = 1;
     }
-
-    // Перенаправляю stdout и захватываю вывод
     char output[1000];
-    FILE* mockstdout = fmemopen(output, sizeof(output), "w");
-    FILE* realstdout = stdout;
-    stdout = mockstdout;
+    // вызываем функцию Generation
+    Generation(t, good);
 
-    // Вызов тестируемой функции
-    Generation(&t, good);
-
-    // Восстанавливаю stdout
-    fflush(stdout);
-    stdout = realstdout;
-
-    // Проверяю, что было сгенерировано 5 паролей длиной 8
-    ASSERT_EQUAL(5, count_occurrences(output, "Password"));
-    ASSERT_EQUAL(40, strlen(strstr(output, "Password")));
-
-    // Проверяю, что все символы в каждом пароле подходят
+    // сравниваем ожидаемый вывод с полученным
     char* p = strtok(output, "\n");
     while (p != NULL) {
-        ASSERT_TRUE(is_good_password(p, t.dlin, good));
+        ASSERT_TRUE(is_good_password(p, t->dlin, good));
         p = strtok(NULL, "\n");
     }
 }
